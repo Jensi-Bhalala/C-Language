dae417e1812601c9c6ae5d39b499a6f8a840bc56
Jensi Bhalala <162539310+Jensi-Bhalala@users.noreply.github.com>
2024-05-22 09:33:39 +0530

    Add files via upload

---
+++ file handling/p-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+{
+	FILE *fp;
+	FILE *fp2;
+	
+	fp = fopen("read.txt","r");
+	
+	if(fp!=NULL)
+	{
+		char msg[20];
+		
+		fscanf(fp,"%[^\n]",&msg);
+		
+		fp2 = fopen("write.txt","w");
+		
+		if(fp2!=NULL)
+		{
+			fprintf(fp2,"%s",msg);
+		}
+	}
+	else
+	{
+		printf("File is Not Open...........");
+	}
+}
