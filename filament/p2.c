44d6bb7a25a22f079f936909be8d9ddbfbf3fcc3
Jensi Bhalala <162539310+Jensi-Bhalala@users.noreply.github.com>
2024-06-28 17:31:43 +0530

    Add files via upload

--- filament/p2.c
+++ filament/p2.c
@@ -1,27 +1,22 @@
-
 #include <stdio.h>
-main(){
-	
-	int i,len;
-	char string[50],freq[128] = {0}; 
-	
-	printf("Enter any string: ");
-    gets(string);
-    
-    len = strlen(string);
-  
-    for (i = 0; i < len; i++)
-	{
-        freq[string[i]]++;
+#define MAX_CHARS 256 
+main() {
+    char str[100];
+    int count[MAX_CHARS] = {0}; 
+    int i;
+
+    printf("Enter any string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        count[str[i]]++;
     }
 
     printf("Frequency of each letter:\n");
-    for ( i = 0; i < 128; i++) 
-	{
-        if (freq[i] > 0) {
-            printf("%c => %d\n", i, freq[i]);
+    for (i = 0; i < MAX_CHARS; i++) {
+        if (count[i] != 0) {
+            printf("%c => %d\n", i, count[i]);
         }
     }
-
 }
 
